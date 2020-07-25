ed581a1f245f349a70f1ec237b0b71f237aa7a38
TS22082 <ts22082@gmail.com>
2020-07-25 10:23:40 -0700

    add comments for clarity

--- controllers/mongo-controller.js
+++ controllers/mongo-controller.js
@@ -8,25 +8,30 @@ const db = mongojs(databaseUrl, collections);
 db.on("error", (err) => console.log({ error_message: err }));
 
 module.exports = {
+  // returns an array even if there are no items or just one
   getNotes: (req, res) =>
     db.notes.find({}, (err, data) => (err ? res.send(err) : res.send(data))),
 
+  // getOne returns one object
   getNote: (req, res) => {
     db.notes.findOne({ _id: mongojs.ObjectID(req.params.id) }, (err, data) => {
       err ? res.send(err) : res.send(data);
     });
   },
 
+  // mongodb allows you to save arrays and objects to the collection
   addNote: (req, res) =>
     db.notes.insert(
       {
         text: req.body.text,
         completed: req.body.completed,
+        // cant do this in sql
         tags: [],
       },
       (err, data) => (err ? res.send(err) : res.send(data))
     ),
 
+  // anytime we are referencing an id you must include mongojs.ObjectID()
   updateText: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
@@ -38,6 +43,7 @@ module.exports = {
   addTag: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
+      // to add a single thing to array
       { $push: { tags: req.body.tag } },
       (err, response) => (err ? res.send(err) : res.send(response))
     );
@@ -46,6 +52,7 @@ module.exports = {
   addTags: (req, res) => {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
+      // to add a multiple things to an array
       { $push: { tags: { $each: req.body.tags } } },
       (err, response) => (err ? res.send(err) : res.send(response))
     );
@@ -55,6 +62,8 @@ module.exports = {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
       {
+        // to pull a single item from an array just use $pull
+        // to pull multiple items use $pullAll
         $pullAll: {
           tags: req.body.tags,
         },
@@ -70,6 +79,8 @@ module.exports = {
     db.notes.update(
       { _id: mongojs.ObjectID(req.body.id) },
       {
+        // to pull a single item from an array just use $pull
+        // to pull multiple items use $pullAll
         $pull: {
           tags: req.body.tag,
         },
@@ -82,6 +93,8 @@ module.exports = {
   },
 
   deleteNote: (req, res) => {
+    // to remove an item from the collection by its id.
+    // any parameter works.
     db.notes.remove({ _id: mongojs.ObjectId(req.params.id) }, (err, data) =>
       err ? res.send(err) : res.send(data)
     );
--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const apiRoutes = require("./routes/test-routes.js");
+// const apiRoutes = require("./routes/test-routes.js");
 const notesRoutes = require("./routes/mongo-routes.js");
-
 app.use(apiRoutes, notesRoutes);
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
